ff372ab0d7629eabdb7992c98e9b3325bb5dc81c
Rajavarshini61 <69765159+Rajavarshini61@users.noreply.github.com>
2026-01-05 15:42:46 +0530

    Update sketch_jan2a_part2.ino
    SCD41 Code with ESP8266 integration and Information displayed in  OLED Display

--- sketch_jan2a_part2/sketch_jan2a_part2.ino
+++ sketch_jan2a_part2/sketch_jan2a_part2.ino
@@ -1,41 +1,83 @@
 #include <Wire.h>
 #include <SensirionI2cScd4x.h>
+#include "SSD1306Ascii.h"
+#include "SSD1306AsciiWire.h"
 
-#define SDA_PIN D3   // GPIO4
-#define SCL_PIN D6   // GPIO5
+// ---------- I2C Pins ----------
+#define SDA_PIN D2   // GPIO4
+#define SCL_PIN D1   // GPIO5
 
+// ---------- I2C Addresses ----------
+#define SCD4X_ADDR SCD41_I2C_ADDR_62
+#define OLED_ADDR  0x3C
+#define RST_PIN    -1
+
+// ---------- Objects ----------
 SensirionI2cScd4x scd4x;
+SSD1306AsciiWire oled;
+
+// ---------- Timing ----------
+const unsigned long MEASURE_INTERVAL = 5000;
+unsigned long lastMeasureTime = 0;
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
 
+  // I2C Init
   Wire.begin(SDA_PIN, SCL_PIN);
-  scd4x.begin(Wire, SCD41_I2C_ADDR_62);
+  Wire.setClock(400000L);
 
+  // OLED Init
+  oled.begin(&Adafruit128x64, OLED_ADDR, RST_PIN);
+  oled.setFont(System5x7);
+  oled.clear();
+  oled.println("SCD41 Init...");
+
+  // SCD41 Init
+  scd4x.begin(Wire, SCD4X_ADDR);
+  delay(30);
+
+  // Start periodic measurement
   scd4x.startPeriodicMeasurement();
+  delay(500);
+
+  oled.println("Measuring...");
   Serial.println("SCD41 connected, measuring...");
 }
 
 void loop() {
+  if (millis() - lastMeasureTime < MEASURE_INTERVAL) return;
+  lastMeasureTime = millis();
+
   uint16_t co2;
-  float temp, hum;
+  float temperature, humidity;
 
-  delay(5000);  // SCD41 updates every ~5 sec
+  // Read sensor (returns 0 on success)
+  if (scd4x.readMeasurement(co2, temperature, humidity) == 0) {
 
-  if (scd4x.readMeasurement(co2, temp, hum) == 0) {
+    // Serial Output
     Serial.print("CO2: ");
     Serial.print(co2);
-    Serial.print(" ppm | ");
-
-    Serial.print("Temp: ");
-    Serial.print(temp);
-    Serial.print(" C | ");
-
-    Serial.print("RH: ");
-    Serial.print(hum);
+    Serial.print(" ppm | Temp: ");
+    Serial.print(temperature, 2);
+    Serial.print(" C | RH: ");
+    Serial.print(humidity, 2);
     Serial.println(" %");
+
+    // OLED Output
+    oled.clear();
+    oled.setCursor(0, 0);
+    oled.print("CO2:  "); oled.print(co2); oled.println(" ppm");
+    oled.print("Temp: "); oled.print(temperature, 1); oled.println(" C");
+    oled.print("RH:   "); oled.print(humidity, 1); oled.println(" %");
+
   } else {
     Serial.println("SCD41 read error");
+
+    oled.clear();
+    oled.setCursor(0, 0);
+    oled.println("SCD41 Error");
   }
 }
+
